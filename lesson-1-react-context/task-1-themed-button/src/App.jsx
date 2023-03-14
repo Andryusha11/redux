4207f4315005f06d2781987a211b8f1e0bdfe820
Andrew Valchuk <andryushahach1999@gmail.com>
2023-03-14 18:06:15 +0200

    add les1 t1

--- lesson-1-react-context/task-1-themed-button/src/App.jsx
+++ lesson-1-react-context/task-1-themed-button/src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import ThemedButton from './ThemedButton';
+import { ThemeContext, themes } from './themes-context';
 
-const App = () => {
-  return <>Hello</>;
-};
+class App extends React.Component {
+  state = {
+    theme: themes.light
+  };
+
+  toggleTheme = () => {
+    const newTheme =
+      this.state.theme === themes.dark ? themes.light : themes.dark;
+
+    this.setState({
+      theme: newTheme
+    });
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>
+            Dynamic Theme
+          </ThemedButton>
+        </ThemeContext.Provider>
+
+        <ThemedButton>Default Theme</ThemedButton>
+      </div>
+    );
+  }
+}
 
 export default App;
