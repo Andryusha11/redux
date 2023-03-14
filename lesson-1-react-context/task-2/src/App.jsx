f4bf5d3f5b0342ad68efa6c3878e538395a2e47d
Andrew Valchuk <andryushahach1999@gmail.com>
2023-03-14 18:48:19 +0200

    add les1 t2

--- lesson-1-react-context/task-2/src/App.jsx
+++ lesson-1-react-context/task-2/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import ObjContext from './context';
+import Header from './Header';
 
-const App = () => {
-  return <>Hello</>;
-};
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001'
+    }
+  };
+  render = () => {
+    return (
+      <div className="page">
+        <ObjContext.Provider value={this.state.userData}>
+          <Header />
+        </ObjContext.Provider>
+      </div>
+    );
+  };
+}
 
 export default App;
---
+++ lesson-1-react-context/task-2/src/context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ObjContext = React.createContext();
+
+export default ObjContext;
